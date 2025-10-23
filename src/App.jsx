0719640c51db4495cc7b8eb116a900b53a4d65f7
hsinha1110 <97324633+hsinha1110@users.noreply.github.com>
2025-10-23 21:37:56 +0530

    JSX Excercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,49 @@
 import "./App.css";
-
-import Login, { Profile, Settings, UserKey } from "./UserComponent";
 function App() {
+  const userObj = {
+    name: "John Doe",
+    age: 30,
+    profession: "Developer",
+  };
+
+  const userArray = ["Alice", "Bob", "Charlie"];
+  let path =
+    "https://images.pexels.com/photos/1108099/pexels-photo-1108099.jpeg?cs=srgb&dl=pexels-chevanon-1108099.jpg&fm=jpg";
+  const name = "React Blog";
+  let x = 10;
+  let y = 20;
+
+  function fruit() {
+    return "Mango";
+  }
+
+  function sum(a, b) {
+    return a + b;
+  }
+
+  function operation(a, b, op) {
+    let result = 0;
+    if (op === "+") {
+      result = a + b;
+    } else if (op === "") {
+      result = a - b;
+    } else {
+      result = a * b;
+    }
+    return result;
+  }
+
   return (
     <>
       <div>
-        <h1>Import and Export Components</h1>
-        <Login />
-        <Profile />
-        <Settings />
-        <h2>User Key: {UserKey}</h2>
+        <h1>JavaScript in JSX</h1>
+        <h1>{name}</h1>
+        <h1>{fruit()}</h1>
+        <h1>{sum(10, 20)}</h1>
+        <h1>{operation(10, 20, "-")}</h1>
+        <h1>{userObj.profession}</h1>
+        <h1>{userArray[0]}</h1>
+        <img src={path} alt="Random" width="600px" />
       </div>
     </>
   );
