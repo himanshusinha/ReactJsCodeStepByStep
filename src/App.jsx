71760796bf2249efa48a849e92449f8fb84c702c
hsinha1110 <97324633+hsinha1110@users.noreply.github.com>
2025-10-31 17:36:00 +0530

    get input value

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,21 @@
 import { useState } from "react";
 import "./App.css";
-import User from "./User";
-import College from "./College";
-import Student from "./Student";
-import Wrapper from "./Wrapper";
+
 function App() {
-  const userObj = {
-    name: "himanshu",
-    age: 35,
-    address: "indore",
-  };
-  const userObj2 = {
-    name: "pooja",
-    age: 32,
-    address: "indore",
-  };
-  const userObj3 = {
-    name: "dakshit",
-    age: 6,
-    address: "indore",
-  };
-  const collegeName = ["DAVV", "SGSITS", "MEDICAPS", "VAISHNAV"];
-
-  const [student, setStudent] = useState("Himanshu");
-
+  const [value, setValue] = useState("");
   return (
     <div>
-      <h1>Props in React Js</h1>
-      {/* <User name={"Himanshu Sinha"} email={"himanshusinha011@gmai.com"} /> */}
-      <User user={userObj} />
-      <User user={userObj2} />
-      <User user={userObj3} />
-
-      <College collegeName={collegeName[0]} />
-      <College collegeName={collegeName[1]} />
-
-      {student && <Student name={student} />}
-      <button onClick={() => setStudent("Dakshit")}>Update Name</button>
-
-      <Wrapper>
-        <h4>Hello</h4>
-      </Wrapper>
-      <Wrapper color="pink">
-        <h4>Hello New User</h4>
-      </Wrapper>
-      <Wrapper color="orange">
-        <h4>Hello {student}</h4>
-      </Wrapper>
+      <h1>Get input field value</h1>
+      <input
+        type="text"
+        placeholder="Enter user name"
+        onChange={(event) => setValue(event.target.value)}
+        value={value}
+      />
+      <br />
+      <br />
+      <button onClick={() => setValue("")}>Clear</button>
+      <h1>{value}</h1>
     </div>
   );
 }
