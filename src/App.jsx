f949b694abc40a1b94d14c79292fdd4406aadb41
hsinha1110 <97324633+hsinha1110@users.noreply.github.com>
2025-11-01 15:59:08 +0530

    checkbox

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,12 @@
 import { useState } from "react";
 import "./App.css";
+import Skills from "./Skills";
 
 function App() {
-  const [name, setName] = useState("");
-  const [password, setPassword] = useState("");
-  const [email, setEmail] = useState("");
-
   return (
     <div>
-      <h1>Controlled Component</h1>
-      <form action="s">
-        <input
-          value={name}
-          type="text"
-          onChange={(event) => setName(event.target.value)}
-          placeholder="Enter user name"
-        />
-        <br />
-        <br />
-        <input
-          value={password}
-          type="password"
-          onChange={(event) => setPassword(event.target.value)}
-          placeholder="Enter user password"
-        />
-        <br />
-        <br />
-        <input
-          value={email}
-          type="text"
-          placeholder="Enter your email"
-          onChange={(event) => setEmail(event.target.value)}
-        />
-        <h1>{name}</h1>
-        <h1>{password}</h1>
-        <h1>{email}</h1>
-
-        <button>Submit</button>
-        <button
-          onClick={() => {
-            setName("");
-            setPassword("");
-            setEmail("");
-          }}
-        >
-          Clear
-        </button>
-      </form>
+      <h1>Handle Checkbox in ReactJS</h1>
+      <Skills />
     </div>
   );
 }
--- src/Skills.jsx
+++ src/Skills.jsx
@@ -1,8 +1,52 @@
+import { useState } from "react";
+
 function Skills() {
+  const [skills, setSkills] = useState([]);
+
+  const handleSkills = (event) => {
+    console.log(event.target.value, event.target.checked);
+
+    if (event.target.checked) {
+      setSkills([...skills, event.target.value]);
+    } else {
+      setSkills(skills.filter((item) => item !== event.target.value));
+    }
+  };
+
   return (
     <div>
       <h1>Select your skills</h1>
-      <input type="checkbox" />
+      <input onChange={handleSkills} type="checkbox" id="php" value="php" />
+      <label htmlFor="php">php</label>
+      <br />
+      <br />
+      <input
+        type="checkbox"
+        onChange={handleSkills}
+        id="javascript"
+        value="javascript"
+      />
+      <label htmlFor="javascript">javascript</label>
+      <br />
+      <br />
+      <input
+        onChange={handleSkills}
+        type="checkbox"
+        id="reactjs"
+        value="reactjs"
+      />
+      <label htmlFor="reactjs">reactjs</label>
+      <br />
+      <br />
+      <input
+        onChange={handleSkills}
+        type="checkbox"
+        id="nodejs"
+        value="nodejs"
+      />
+      <label htmlFor="nodejs">nodejs</label>
+      <h2>{skills.length > 0 ? skills.join(", ") : ""}</h2>
     </div>
   );
 }
+export default Skills;
