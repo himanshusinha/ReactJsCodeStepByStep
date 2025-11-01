ec61e6c50bdc26c0098756c7fd9793e7ceee84bc
hsinha1110 <97324633+hsinha1110@users.noreply.github.com>
2025-11-01 15:10:37 +0530

    controlled component

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,51 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [value, setValue] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+
   return (
     <div>
-      <h1>Get input field value</h1>
-      <input
-        type="text"
-        placeholder="Enter user name"
-        onChange={(event) => setValue(event.target.value)}
-        value={value}
-      />
-      <br />
-      <br />
-      <button onClick={() => setValue("")}>Clear</button>
-      <h1>{value}</h1>
+      <h1>Controlled Component</h1>
+      <form action="s">
+        <input
+          value={name}
+          type="text"
+          onChange={(event) => setName(event.target.value)}
+          placeholder="Enter user name"
+        />
+        <br />
+        <br />
+        <input
+          value={password}
+          type="password"
+          onChange={(event) => setPassword(event.target.value)}
+          placeholder="Enter user password"
+        />
+        <br />
+        <br />
+        <input
+          value={email}
+          type="text"
+          placeholder="Enter your email"
+          onChange={(event) => setEmail(event.target.value)}
+        />
+        <h1>{name}</h1>
+        <h1>{password}</h1>
+        <h1>{email}</h1>
+
+        <button>Submit</button>
+        <button
+          onClick={() => {
+            setName("");
+            setPassword("");
+            setEmail("");
+          }}
+        >
+          Clear
+        </button>
+      </form>
     </div>
   );
 }
---
+++ src/Skills.jsx
@@ -0,0 +1,8 @@
+function Skills() {
+  return (
+    <div>
+      <h1>Select your skills</h1>
+      <input type="checkbox" />
+    </div>
+  );
+}
