eb639dec0bd7e2a5c880f7c10abd1380a6e555b0
hsinha1110 <97324633+hsinha1110@users.noreply.github.com>
2025-11-06 07:07:47 +0530

    dynamic conditional and inline style

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,128 @@
-import { useEffect, useState } from "react";
-import "./App.css";
-import Counter from "./Counter";
+import { useState } from "react";
+
 function App() {
-  const [count, setCount] = useState(0);
-  const [data, setData] = useState(0);
-  const [display, setDisplay] = useState(true);
+  const [cardStyle, setCardStyle] = useState({
+    border: "1px solid #ccc",
+    width: "200px",
+    boxShadow: "1px 2px 3px 0px #cccccc57",
+    margin: "10px",
+  });
+  const [textColor, setTextColor] = useState("black");
+  const [grid, setGrid] = useState(true);
+
+  const imageStyle = {
+    width: "200px",
+  };
+
+  const textContainerStyle = {
+    padding: "5px",
+    color: textColor,
+  };
+  const updateTheme = (bgColor, textColor) => {
+    setCardStyle({ ...cardStyle, backgroundColor: bgColor });
+    setTextColor(textColor);
+  };
   return (
     <div>
-      {display ? <Counter count={count} data={data} /> : null}
-      <p>Count: {count}</p>
-      <button onClick={() => setCount(count + 1)}>Counter</button>
-      <button onClick={() => setData(data + 1)}>Data</button>
-      <button onClick={() => setDisplay(!display)}>Toggle</button>
+      <h1 style={{ color: "red" }}>
+        Dynamic Conditional and Inline Style in React
+      </h1>
+      <button onClick={() => updateTheme("gray", "green")}>Gray Theme</button>
+      <button onClick={() => updateTheme("white", "black")}>
+        Default Theme
+      </button>
+      <button onClick={() => setGrid(!grid)}>Toggle</button>
+      <div style={{ display: grid ? "block" : "flex", flexWrap: "wrap" }}>
+        <div style={cardStyle}>
+          <img
+            style={imageStyle}
+            src="https://www.w3schools.com/howto/img_avatar.png"
+            alt=""
+          />
+          <div style={textContainerStyle}>
+            <h4>Anil Sidhu</h4>
+            <p>Software Developer</p>
+          </div>
+        </div>
+
+        <div style={cardStyle}>
+          <img
+            style={imageStyle}
+            src="https://www.w3schools.com/howto/img_avatar.png"
+            alt=""
+          />
+          <div style={textContainerStyle}>
+            <h4>Anil Sidhu</h4>
+            <p>Software Developer</p>
+          </div>
+        </div>
+        <div style={cardStyle}>
+          <img
+            style={imageStyle}
+            src="https://www.w3schools.com/howto/img_avatar.png"
+            alt=""
+          />
+          <div style={textContainerStyle}>
+            <h4>Anil Sidhu</h4>
+            <p>Software Developer</p>
+          </div>
+        </div>
+        <div style={cardStyle}>
+          <img
+            style={imageStyle}
+            src="https://www.w3schools.com/howto/img_avatar.png"
+            alt=""
+          />
+          <div style={textContainerStyle}>
+            <h4>Anil Sidhu</h4>
+            <p>Software Developer</p>
+          </div>
+        </div>
+        <div style={cardStyle}>
+          <img
+            style={imageStyle}
+            src="https://www.w3schools.com/howto/img_avatar.png"
+            alt=""
+          />
+          <div style={textContainerStyle}>
+            <h4>Anil Sidhu</h4>
+            <p>Software Developer</p>
+          </div>
+        </div>
+        <div style={cardStyle}>
+          <img
+            style={imageStyle}
+            src="https://www.w3schools.com/howto/img_avatar.png"
+            alt=""
+          />
+          <div style={textContainerStyle}>
+            <h4>Anil Sidhu</h4>
+            <p>Software Developer</p>
+          </div>
+        </div>
+        <div style={cardStyle}>
+          <img
+            style={imageStyle}
+            src="https://www.w3schools.com/howto/img_avatar.png"
+            alt=""
+          />
+          <div style={textContainerStyle}>
+            <h4>Anil Sidhu</h4>
+            <p>Software Developer</p>
+          </div>
+        </div>
+        <div style={cardStyle}>
+          <img
+            style={imageStyle}
+            src="https://www.w3schools.com/howto/img_avatar.png"
+            alt=""
+          />
+          <div style={textContainerStyle}>
+            <h4>Anil Sidhu</h4>
+            <p>Software Developer</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
