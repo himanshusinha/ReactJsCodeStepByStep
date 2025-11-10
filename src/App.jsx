a711e04afdb30bd412b21eb3bd5ff1392ae144ee
hsinha1110 <97324633+hsinha1110@users.noreply.github.com>
2025-11-10 17:47:19 +0530

    uncontrolled component in reactjs

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,58 @@ import { useRef } from "react";
 import "./css/style.css";
 
 function App() {
-  const inputRef = useRef();
-  const h1Ref = useRef();
-  const inputHandler = () => {
-    console.log("input ref value");
-    inputRef.current.focus();
-    inputRef.current.style.color = "red";
-    inputRef.current.placeholder = "Enter password";
-    inputRef.current.value = "123";
+  const userRef = useRef();
+  const passwordRef = useRef();
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const user = document.querySelector("#user").value;
+    const password = document.querySelector("#password").value;
+    console.log(user, password);
   };
-  const h1Handler = () => {
-    h1Ref.current.style.color = "green";
+  const handleSubmitRef = (event) => {
+    event.preventDefault();
+    const user = userRef.current.value;
+    const password = passwordRef.current.value;
+    console.log("Ref values:", user, password);
   };
-  const toggleHandler = () => {
-    if (inputRef.current.style.display !== "none") {
-      inputRef.current.style.display = "none";
-    } else {
-      inputRef.current.style.display = "inline";
-    }
-  };
-
   return (
     <div>
-      <h1>useRef in ReactJS</h1>
-      <button onClick={inputHandler}>Change Value</button>
-      <input ref={inputRef} type="text" placeholder="Enter your name" />
-      <button onClick={toggleHandler}>Toggle Visibility</button>
-      <h1 ref={h1Ref}>React JS</h1>
-      <button onClick={h1Handler}>Change h1 Style</button>
+      <h1>Uncontrolled component in ReactJS</h1>
+      <form action="" onSubmit={handleSubmit}>
+        <input type="text" placeholder="enter your name" id="user" />
+        <br />
+        <br />
+        <input
+          type="password"
+          placeholder="enter your password"
+          id="password"
+        />
+        <br />
+        <br />
+        <button>Submit</button>
+        <br />
+        <br />
+      </form>
+
+      <form action="" onSubmit={handleSubmitRef}>
+        <input
+          ref={userRef}
+          type="text"
+          placeholder="enter your name"
+          id="userRef"
+        />
+        <br />
+        <br />
+        <input
+          ref={passwordRef}
+          type="password"
+          placeholder="enter your password"
+          id="passwordRef"
+        />
+        <br />
+        <br />
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
