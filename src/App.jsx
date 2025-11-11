10547b255738481724e81e303c2b32983640ea6a
hsinha1110 <97324633+hsinha1110@users.noreply.github.com>
2025-11-11 11:43:08 +0530

    useTransitionHook

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,26 @@
+import { useState, useTransition } from "react";
 import "./css/style.css";
-import { useFormStatus } from "react-dom";
 function App() {
-  const handleSubmit = async () => {
-    await new Promise((res) => setTimeout(res, 5000));
-    console.log("submitted");
-  };
-  const CustomerForm = () => {
-    const { pending } = useFormStatus();
-    console.log(pending);
-    return (
-      <div>
-        <h1>useFormStatus Hook in React JS 19</h1>
-        <input type="text" placeholder="Enter name" />
-        <br />
-        <br />
-        <input type="password" placeholder="Enter password" />
-        <br />
-        <br />
-        <button type="submit" disabled={pending}>
-          {pending ? "Submitting..." : "Submit"}
-        </button>
-      </div>
-    );
+  const [pending, setPending] = useTransition();
+  const handleSubmit = () => {
+    setPending(async () => {
+      await new Promise((res) => setTimeout(res, 2000));
+    });
   };
   return (
-    <form action={handleSubmit}>
-      <CustomerForm />
-    </form>
+    <div>
+      <h1>useTransition Hook in ReactJS 19</h1>
+      {pending ? (
+        <img
+          style={{ width: "100px" }}
+          src="https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif"
+        />
+      ) : null}
+      <br />
+      <button disabled={pending} onClick={handleSubmit}>
+        Submit
+      </button>
+    </div>
   );
 }
 
