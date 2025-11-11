cbca8aae9b46a54bdb47db052390bdd1338208a2
hsinha1110 <97324633+hsinha1110@users.noreply.github.com>
2025-11-11 12:30:51 +0530

    Derived State

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,28 @@
+import { useState } from "react";
 import "./css/style.css";
 function App() {
+  const [users, setUsers] = useState([]);
+  const [user, setUser] = useState("");
+  const handleUsers = () => {
+    setUsers([...users, user]).length;
+  };
+
+  const totalUser = users.length;
+  const lastUser = users[users.length - 1];
+  const uniqueUser = [...new Set(users)].length;
   return (
     <div>
-      <h1>Pure Component in ReactJS 19</h1>
-      <Counter count={1} />
-      <Counter count={2} />
-      <Counter count={3} />
-      <Counter count={4} />
+      <h1>Derived State in ReactJS 19</h1>
+      <h4>Total User : {totalUser}</h4>
+      <h4>Last User : {lastUser}</h4>
+      <h4>Total Unique User : {uniqueUser}</h4>
+
+      <input type="text" onChange={(event) => setUser(event.target.value)} />
+      <button onClick={handleUsers}>Add Users</button>
+      {users.map((item, index) => {
+        return <h4 key={index}>{item}</h4>;
+      })}
     </div>
   );
 }
-const Counter = ({ count }) => {
-  return (
-    <div>
-      <h1>Counter : {count}</h1>
-    </div>
-  );
-};
 export default App;
