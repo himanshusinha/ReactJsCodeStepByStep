02206027f9cbc61ccaee6a01f04809a25a0860bb
hsinha1110 <97324633+hsinha1110@users.noreply.github.com>
2025-11-11 12:03:29 +0530

    Pure Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,20 @@
-import { useState, useTransition } from "react";
 import "./css/style.css";
 function App() {
-  const [pending, setPending] = useTransition();
-  const handleSubmit = () => {
-    setPending(async () => {
-      await new Promise((res) => setTimeout(res, 2000));
-    });
-  };
   return (
     <div>
-      <h1>useTransition Hook in ReactJS 19</h1>
-      {pending ? (
-        <img
-          style={{ width: "100px" }}
-          src="https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif"
-        />
-      ) : null}
-      <br />
-      <button disabled={pending} onClick={handleSubmit}>
-        Submit
-      </button>
+      <h1>Pure Component in ReactJS 19</h1>
+      <Counter count={1} />
+      <Counter count={2} />
+      <Counter count={3} />
+      <Counter count={4} />
     </div>
   );
 }
-
+const Counter = ({ count }) => {
+  return (
+    <div>
+      <h1>Counter : {count}</h1>
+    </div>
+  );
+};
 export default App;
