7be0ac5be90bed87f85aeeafaa59905435e404b6
hsinha1110 <97324633+hsinha1110@users.noreply.github.com>
2025-11-12 19:46:24 +0530

    Fragment

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,22 @@
-import { useId } from "react";
 import "./css/style.css";
 
 function App() {
   return (
-    <div>
-      <UserForm />
-      <hr />
-      <UserForm />
-    </div>
-  );
-}
-function UserForm() {
-  const name = useId();
-  const email = useId();
-  const password = useId();
-  const terms = useId();
-  return (
-    <div>
-      <form action="">
-        <label htmlFor={name}>Enter User Name</label>
-        <input type="text" placeholder="Enter user name" id={name} />
-        <br />
-        <br />
-        <label htmlFor={email}>Enter User Name</label>
-        <input type="email" placeholder="Enter user email" id={email} />
-        <br />
-        <br />
-        <label htmlFor={password}>Enter User Name</label>
-        <input
-          type="password"
-          placeholder="Enter user password"
-          id={password}
-        />
-        <br />
-        <br />
-        <input type="checkbox" value="Terms and Condition" id={terms} />
-        <label htmlFor={terms}>Terms and Condition</label>
-      </form>
-    </div>
+    <>
+      <Data />
+      <Data />
+      <Data />
+      <Data />
+    </>
   );
 }
 
+function Data() {
+  return (
+    <>
+      <h1>Fragment in React JS</h1>
+      <h1>Fragment in React JS</h1>
+    </>
+  );
+}
 export default App;
