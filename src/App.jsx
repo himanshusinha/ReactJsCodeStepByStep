ba8a551083258f0a975fa6026bca4529ba4fcd35
hsinha1110 <97324633+hsinha1110@users.noreply.github.com>
2025-11-12 17:20:25 +0530

    useId Hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,41 @@
-import { useActionState } from "react";
+import { useId } from "react";
 import "./css/style.css";
 
 function App() {
-  const handleSubmit = async (previousData, formData) => {
-    const name = formData.get("name");
-    const password = formData.get("password");
-    console.log("handleSubmit data", name, password);
-    await new Promise((res) => setTimeout(res, 3000));
-
-    if (name && password) {
-      return { message: "Data Submitted", name, password };
-    } else {
-      return { error: "Failed to Submit. Enter proper data", name, password };
-    }
-  };
-
-  const [data, action, pending] = useActionState(handleSubmit, {});
-
   return (
     <div>
-      <h1>useActionState Hook in React 19</h1>
-      <form action={action}>
-        <input
-          defaultValue={data?.name}
-          type="text"
-          name="name"
-          placeholder="Enter name"
-        />
+      <UserForm />
+      <hr />
+      <UserForm />
+    </div>
+  );
+}
+function UserForm() {
+  const name = useId();
+  const email = useId();
+  const password = useId();
+  const terms = useId();
+  return (
+    <div>
+      <form action="">
+        <label htmlFor={name}>Enter User Name</label>
+        <input type="text" placeholder="Enter user name" id={name} />
         <br />
         <br />
+        <label htmlFor={email}>Enter User Name</label>
+        <input type="email" placeholder="Enter user email" id={email} />
+        <br />
+        <br />
+        <label htmlFor={password}>Enter User Name</label>
         <input
-          defaultValue={data?.password}
           type="password"
-          name="password"
-          placeholder="Enter password"
+          placeholder="Enter user password"
+          id={password}
         />
         <br />
         <br />
-        <button disabled={pending}>
-          {pending ? "Submitting..." : "Submit Data"}
-        </button>
-        <br />
-        <br />
-        {data.error && <span style={{ color: "red" }}>{data.error}</span>}
-        {data.message && <span style={{ color: "green" }}>{data.message}</span>}
-
-        <h4>Name: {data.name || ""}</h4>
-        <h4>Password: {data.password || ""}</h4>
+        <input type="checkbox" value="Terms and Condition" id={terms} />
+        <label htmlFor={terms}>Terms and Condition</label>
       </form>
     </div>
   );
